88d23f5e7130158a63e64846cb5eab136ca769ed
Abril31 <brivaldi0416@gmail.com>
2023-10-26 23:34:50 -0400

    Configuraciones Front

---
+++ client/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,23 @@
+import "./NavBar.css";
+import React from "react";
+import { Link } from "react-router-dom";
+import SearchBar from "../SearchBar/SearchBar";
+
+const NavBar = () => {
+  return (
+    <div className="nav-container">
+      <Link to="/home">
+        <h2>Home</h2>
+      </Link>
+      <Link to="/form">
+        <h2>Create</h2>
+      </Link>
+      <Link>
+        <h2>History</h2>
+      </Link>
+      <SearchBar />
+    </div>
+  );
+};
+
+export default NavBar;
---
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+const SearchBar = () => {
+  return (
+    <div>
+      <form>
+        <input type="text" />
+        <button>Search</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchBar;
